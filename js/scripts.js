d88e881f96b88685e82b764108beb9ecf0ae843b
SkyeN <skye@dames.es>
2018-08-15 11:32:21 -0700

    fix backend logic to deal with y

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-var vowels = ["a","e","i","o","u","y"];
+var vowels;
 
+var yChecker = function (input) {
+  if (input.charAt(0) === "y" || input.charAt(0) === "Y") {
+    vowels = ["a","e","i","o","u", "A", "E", "I", "O", "U"];
+  }
+  else {
+    vowels = ["a","e","i","o","u", "y", "A", "E", "I", "O", "U", "Y"];
+  }
+}
 var letterChecker = function (inputStr) {
   if (inputStr.match(/^[a-zA-Z\s]+$/)) {
     return true;
@@ -22,25 +30,23 @@ var moveConsonants = function (input) {
       index++;
     }
   }
-  consonantSubStr = input.substring(0,startOfVowel);
-  resultWord = input.substring(startOfVowel,input.length) + consonantSubStr +"ay";
+  consonantSubStr = input.substring(0, startOfVowel);
+  resultWord = input.substring(startOfVowel, input.length) + consonantSubStr + "ay";
   return resultWord;
 }
 
-var vowelChecker = function(input){
+var vowelChecker = function (input) {
   var resultWord = input;
-  var consonantSubStr ="";
-
-  if (input.charAt(0) === "y" || "Y") {
-    vowels = ["a","e","i","o","u"];
-    return moveConsonants(input);
-  }
-  if(vowels.includes(input.charAt(0))) {
+  var consonantSubStr = "";
+  yChecker(input);
+  
+  if (vowels.includes(input.charAt(0))) {
     resultWord += "way";
   }
-  else if (input.substring(0,2) ===  "qu" || "QU" || "Qu" || "qU") {
-    consonantSubStr = input.substring(0,2);
-    resultWord = input.substring(2,input.length) +consonantSubStr+"ay";
+  
+  else if (input.substring(0,2) ===  "qu" || input.substring(0,2) ===  "QU" || input.substring(0,2) ===  "Qu" || input.substring(0,2) ===  "qU") {
+    consonantSubStr = input.substring(0, 2);
+    resultWord = input.substring(2, input.length) + consonantSubStr + "ay";
   }
 
   else if (!vowels.includes(input.charAt(0))) {
@@ -49,16 +55,15 @@ var vowelChecker = function(input){
   return resultWord;
 }
 
-$(document).ready(function(){
-  $("#input").submit(function(event){
+$(document).ready(function () {
+  $("#input").submit(function (event) {
     event.preventDefault();
-
     $("#result").empty();
     var inputStr = $("#inputStr").val().trim();
-    if (!letterChecker(inputStr)) { alert ("Please enter a valid sentence!")}
+    if (!letterChecker(inputStr)) { alert("Please enter a valid sentence!") }
     var input = inputStr.split(" ");
-    for (var i=0; i<input.length;i++) {
-      $("#result").append(" "+vowelChecker(input[i]));
+    for (var i = 0; i < input.length; i++) {
+      $("#result").append(" " + vowelChecker(input[i]));
     }
 
   });
