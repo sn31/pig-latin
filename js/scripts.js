b79f374af54ebe5ea500590ba3db7a1068226f5b
Hyewon Cho, Meria Thomas and Skye Nguyen <skye@>
2018-08-15 09:14:56 -0700

    validate input string using regex

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,35 @@
 var vowels = ["a","e","i","o","u"];
 
+var letterChecker = function (inputStr) {
+  if (inputStr.match(/^[a-zA-Z\s]+$/)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 var vowelChecker = function(input){
   var resultWord = input;
   if(vowels.includes(input.charAt(0))) {
     resultWord += "way";
   }
+
   return resultWord;
 }
 
 $(document).ready(function(){
   $("#input").submit(function(event){
     event.preventDefault();
+
     $("#result").empty();
     var inputStr = $("#inputStr").val();
+    if (!letterChecker(inputStr)) { alert ("Please enter a valid sentence!")}
     var input = inputStr.split(" ");
     for (var i=0; i<input.length;i++) {
       $("#result").append(" "+vowelChecker(input[i]));
     }
 
   });
+  console.log(letterChecker("hi my name is sss"));
+
 });
